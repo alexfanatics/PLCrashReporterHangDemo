fbe16aeeb21e001c5cfc41a52287a9196615cda8
Ahmed Ashraf <aashraf@instabug.com>
2024-01-31 14:36:59 +0200

    init project

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import shared
+
+struct ContentView: View {
+	let greet = Greeting().greet()
+
+	var body: some View {
+		Text(greet)
+            .onTapGesture {
+                _ = [32][3]
+            }
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
